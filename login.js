6bd409f929790164a640860513cec9c5cccfa9fc
talafarael <127351345+talafarael@users.noreply.github.com>
2023-11-19 21:08:58 +0200

    Update login.js

--- login.js
+++ login.js
@@ -27,6 +27,7 @@ loginForm.addEventListener("submit", async (event) => {
   // Опции запроса
   const options = {
     method: "POST",
+    credentials:'include',
     headers: {
       "Content-Type": "application/json",
       // Другие необходимые заголовки могут быть добавлены здесь
